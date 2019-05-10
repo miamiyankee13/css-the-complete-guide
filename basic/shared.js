f75d55c41bd3defec7e1539db28285dec30d1658
Anthony Damico <anthony.damico721@gmail.com>
2019-05-10 14:15:45 -0400

    completed lecture 136

--- basic/shared.js
+++ basic/shared.js
@@ -3,8 +3,10 @@ let backdrop = document.querySelector('.backdrop');
 let modal = document.querySelector('.modal');
 let modalNoButton = document.querySelector('.modal__action--negative')
 let selectPlanButtons = document.querySelectorAll('.plan button');
+let toggleButton = document.querySelector('.toggle-button');
+let mobileNav = document.querySelector('.mobile-nav');
 
-//loop through buttons to add click event listeners
+//loop through plan buttons to add click event listeners
 for (let i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function() {
         modal.style.display = 'block';
@@ -12,12 +14,23 @@ for (let i = 0; i < selectPlanButtons.length; i++) {
     });
 }
 
-//declare function to close modal
+//declare function to close modal & backdrop
 function closeModal() {
     modal.style.display = 'none';
     backdrop.style.display = 'none';
 }
 
-//add click event listeners to backdrop and modal "no" button
-backdrop.addEventListener('click', closeModal);
+//add click event listeners to backdrop
+backdrop.addEventListener('click', function() {
+    mobileNav.style.display = 'none';
+    closeModal();
+});
+
+//add click event listener to modal "no" button
 modalNoButton.addEventListener('click', closeModal);
+
+//add click event listener to toggle button
+toggleButton.addEventListener('click', function() {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+});
